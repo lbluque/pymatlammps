6f56a203c25160c3fac701a0aa9b5f08ff806b93
lbluque <lbluque@berkeley.edu>
2020-10-19 09:31:47 -0700

    actually adding the setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyMatLammps',
+    version='0.0.0',
+    url='https://github.com/lbluque/pymatlammps',
+    author='Luis Barroso-Luque',
+    author_email='lbluque@berkeley.edu',
+    description='Minimal wrapper to interface pymatgen and PyLammps',
+    packages=find_packages(),
+    install_requires=['numpy >= 1.18.0', 'pymatgen >= 2020.10.9'],
+)
